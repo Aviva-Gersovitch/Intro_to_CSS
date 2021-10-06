21464aac707d0cb47cc4372dddc83aaf48f06ae8
Aviva Gersovitch <ahg42@case.edu>
2021-10-06 12:16:01 -0400

    Adding Intro to JS Day code

---
+++ script.js
@@ -0,0 +1,81 @@
+// //logging messages to the console//
+
+// console.log ("hello world!");
+
+//declaring a variable//
+
+// let x = 5;
+// console.log (x);
+
+// const my_fav_num = 18;
+
+//String Data Types //
+// let my_string = "example string";
+// let my_str_num = "7";
+
+//Number Datatype//
+
+
+//Boolean Datatype// 
+
+// let amountRaisedSoFar = 1000;
+
+// let donation = prompt("How much would you like to donate?"); 
+
+// amountRaisedSoFar = Number(donation) + amountRaisedSoFar;
+
+// console.log("We have now raised: " + amountRaisedSoFar + "!");
+
+// let cups_consumed = 4;
+// if cups_consumed < 3 {
+//     console.log ("Yes, I'll take another cup of coffee");
+//     }
+//     else {console.log ("I think I'm okay for now.");
+// }
+
+// somevar && myFunction() //equivalant to if(somevar)
+// somevar || myFunction() //equivalant to if(!somevar)
+
+// let temp = 80;
+//     console.log("The temperature is ${temp}."); //Change to backticks?
+
+// if temp > 80 {
+//     console.log("time to swim");
+// }
+
+// let precip = false;
+
+// if temp > 80 && precip = false {
+//     console.log ("time to swim");
+// }
+
+
+// let indoors = true;
+
+// if indoors = true {
+//     console.log ("time to swim");
+// }
+    
+
+// for (let bottles = 99; bottles >=0; bottles--) {
+//     console.log(bottles);
+// }
+//     console.log("Hey, we need more root beer!");
+
+// let y =1;
+// switch (y){
+//     case 3: console.log(3);
+//     case 1: console.log(1);
+//     case 2: console.log(2);
+//         break;
+//     default: console.log('default');
+// }
+
+// for (var i = 0; i < 10; i++){
+//     console.log(i); //
+//   }
+//   console.log('outside of code block', i); // will log 9, the final value of i in the for loop, 
+//               //because var declared it globally. if you used let it wouldn't have access to the variable outside of the {}
+
+let miles = 501;
+    if miles = 
